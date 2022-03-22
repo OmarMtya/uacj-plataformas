f2f932d7c20ebf085585a01367d875edf8e43b0d
Omar Montoya <luis.montoya.j@gmail.com>
2022-03-22 09:04:57 -0600

    Nuevo link de redireccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   server: (plataforma: string) => `https://indicadores.uacj.mx/${plataforma}/public`,
-  redirectMSAL: 'https://indicadores.uacj.mx/indicapp/login'
+  redirectMSAL: 'https://indicadores.uacj.mx/indicapp/#/login'
 };
